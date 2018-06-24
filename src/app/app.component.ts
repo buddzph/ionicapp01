0cbf8199c11a8919752b5371638c36991a987d82
Frederick de Guzman <frederickdeguzman@gmail.com>
2018-06-24 19:33:29 +0800

    About Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild('content') nav: NavController;
   rootPage:any = HomePage;
+  pages: Array<{title: string, icon: string, component: any}>;
+  item: Array<{name: 'dog', age: 1}>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +21,16 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    this.pages = [
+      { title: 'Home', icon: 'home', component: HomePage },
+      { title: 'About', icon: 'list-box', component: AboutPage }
+    ];
   }
+
+  openPage(p) {
+    // console.log('Page: ', p);
+    this.nav.setRoot(p.title);
+  } 
 }
 
